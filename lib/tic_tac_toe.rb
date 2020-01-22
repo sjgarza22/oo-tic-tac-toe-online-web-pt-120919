523887bd0c74f157eb40884b6f0ce901d6984bac
Sara Garza <sjgarza22@gmail.com>
2020-01-22 01:30:49 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,25 +1,25 @@
 class TicTacToe
-  
+
   attr_accessor :board
-  
+
   WIN_COMBINATIONS = []
 
   def initialize()
     @board = Array.new(9, " ")
-  
+
   end
 
   def display_board
-  
+
   end
-  
+
   def input_to_index(index)
-  
+
   end
-  
+
   def move(index, token)
-  
+
   end
-  
-  
+
+
 end
